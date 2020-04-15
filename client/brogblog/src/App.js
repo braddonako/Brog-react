2e49c6a99d9e309df85f2d1e8e08d190cc8d8427
Brad Donakowski <braddonako@gmail.com>
2020-04-15 17:01:59 -0600

    didnt mean to commit on server

--- client/brogblog/src/App.js
+++ client/brogblog/src/App.js
@@ -1,8 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
-
-
 class App extends Component {
 
   componentDidMount(){
@@ -12,11 +10,11 @@ class App extends Component {
   }
 
   render(){
-  return (
-    <div className="App">
-      MY APP A HAHAHAHAH
-    </div>
-  )};
+    return (
+       <div className="App">
+          MY APP A HAHAHAHAH
+        </div>
+      )};
 }
 
 export default App;
--- client/brogblog/src/setupProxy.js
+++ client/brogblog/src/setupProxy.js
@@ -1,4 +1,8 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// https://create-react-app.dev/docs/proxying-api-requests-in-development/#configuring-the-proxy-manually
+// link to proxying in react -- tried to proxy through package.json, and this was the solution
+
 module.exports = function(app) {
   app.use(
       '/api/',
