66b6154e8369b399d9e78f696cce7a25859f6d15
Brad Donakowski <braddonako@gmail.com>
2020-04-15 07:10:55 -0600

    forgot creating a logout route. Just added this in

--- server/server.js
+++ server/server.js
@@ -80,6 +80,19 @@ app.post('/api/users/login', (req, res)=>{
     })
 });
 
+app.get('/api/users/logout', auth, (req,res)=>{
+    User.findOneAndUpdate(
+    {_id: req.user._id}, 
+    {token: ''},
+    (err, doc) => {
+        if (err) return res.json({success: false, err})
+        return res.status(200).send({
+            success: true
+        })
+    }
+    )
+})
+
 const port = process.env.PORT || 3000;
 
 app.listen(3000, ()=> {
