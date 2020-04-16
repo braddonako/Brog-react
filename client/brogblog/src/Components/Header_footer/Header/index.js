e5ef8dc9b22d3932f87b0c0ac4abe8d52a79879e
Brad Donakowski <braddonako@gmail.com>
2020-04-16 14:22:09 -0600

    added the header with the hamburger

--- client/brogblog/src/Components/Header_footer/Header/index.js
+++ client/brogblog/src/Components/Header_footer/Header/index.js
@@ -5,7 +5,7 @@ import Toolbar from '@material-ui/core/Toolbar';
 import MenuIcon from '@material-ui/icons/Menu';
 import IconButton from '@material-ui/core/IconButton';
 
-import SideDrawer from './SideDrawer'
+import SideDrawer from './sideDrawer'
 
 class Header extends Component {
 
@@ -49,8 +49,8 @@ class Header extends Component {
            >
                <Toolbar>
                    <div className='header_'>
-                        <div className='font_righteous header_logo_venue'>The Venue</div>
-                        <div className='header_logo_title'>Musical Events</div>
+                        <div className='font_righteous header_logo_venue'>BROG</div>
+                        <div className='header_logo_title'>AKA Brads Blog</div>
                    </div>
 
                 <div style={{right: '0',
