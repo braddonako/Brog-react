af0ba3680d2c6ed0f2dd8432fd5d00fc1f2d0593
Brad Donakowski <braddonako@gmail.com>
2020-04-16 19:51:37 -0600

    going to add a register and login component

--- client/brogblog/src/routes.js
+++ client/brogblog/src/routes.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import {Switch, Route } from 'react-router-dom'
+import {Switch, Route } from 'react-router-dom';
 
+import RegisterLogin from './Components/Register_login'
 import Home from './Components/Home';
 import Layout from './hoc/layout'
 
@@ -8,6 +9,7 @@ const Routes = () => {
   return(
     <Layout>
       <Switch>
+        <Route path='/register_login' exact component={ RegisterLogin }/>
         <Route path='/' exact component={ Home }/>
       </Switch>
     </Layout>
