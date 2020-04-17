77bb9f6e604436b0e380129d7746b2adcd69a95b
Brad Donakowski <braddonako@gmail.com>
2020-04-16 20:53:28 -0600

    Updated info on the footer

--- client/brogblog/src/Components/Header_footer/Footer/index.js
+++ client/brogblog/src/Components/Header_footer/Footer/index.js
@@ -15,60 +15,66 @@ const Footer = () => {
                 <div className='wrapper'>
                     <div className='left'>
                         <h2>Contact Information</h2>
-                        <div className='business_nfo'>
+                            <div className='business_nfo'>
+                                <div className='tag'>
+                                    <FontAwesomeIcon 
+                                    icon={faCompass}
+                                    className='icon'
+                                    />
+                                    <div className='nfo'>
+                                        <div>Location</div>
+                                        <div>Denver, CO</div>
+                                    </div>
+                                </div>
+
                             <div className='tag'>
-                                <FontAwesomeIcon 
-                                icon={faCompass}
-                                className='icon'
-                                />
+                                <a href = 'https://www.linkedin.com/in/braddonakowski/' className='linkedIn'>
+                                    <FontAwesomeIcon 
+                                    icon={faLinkedinIn}
+                                    className='icon'
+                                    />
+                                </a>
                                 <div className='nfo'>
-                                    <div>Location</div>
-                                    <div>Denver, CO</div>
+                                    <div>Click the logo to</div>
+                                    <div>Connect with me on LinkedIn</div>
                                 </div>
                             </div>
 
-                        <div className='tag'>
-                            <a href = 'https://www.linkedin.com/in/braddonakowski/' className='linkedIn'>
-                                <FontAwesomeIcon 
-                                icon={faLinkedinIn}
-                                className='icon'
-                                />
-                            </a>
-                            <div className='nfo'>
-                                <div>Click the logo to</div>
-                                <div>Connect with me on LinkedIn</div>
+                            <div className='tag'>
+                                <a href='https://www.github.com/braddonako' className='github'>
+                                    <FontAwesomeIcon 
+                                    icon={faGithub}
+                                    className='icon'
+                                    />
+                                </a>
+                                <div className='nfo'>
+                                    <div>Click the logo to</div>
+                                    <div>access my Github</div>
+                                </div>
                             </div>
-                        </div>
-
-                        <div className='tag'>
-                            <a href='https://www.github.com/braddonako' className='github'>
-                                <FontAwesomeIcon 
-                                icon={faGithub}
-                                className='icon'
-                                />
-                            </a>
-                            <div className='nfo'>
-                                <div>Click the logo to</div>
-                                <div>access my Github</div>
-                            </div>
-                        </div>
-                        
-                        <div className='tag'>
-                                <FontAwesomeIcon 
-                                icon={faEnvelope}
-                                className='icon'
-                                />
+                            
+                            <div className='tag'>
+                                    <FontAwesomeIcon 
+                                    icon={faEnvelope}
+                                    className='icon'
+                                    />
                                 <div className='nfo'>
                                     <div>Email</div>
                                     <div>braddonakowski@yahoo.com</div>
                                 </div>
-                        </div>
-                        </div>                      
+                            </div>
+                        </div>     
+
                     </div>
                     <div className='left'>
-                        
+                        <h2>Share your thoughts</h2>
+                        <div>
+                            <div>I love receiving feedback, and am always open to suggestions. If you have a topic you would like me to discuss, just want to chat, or you want to grab a cup of coffee (aka 'fee) shoot me a message!</div>
+                        </div>
                     </div>
+                    
                 </div>
+
             </div>
             
         </footer>
