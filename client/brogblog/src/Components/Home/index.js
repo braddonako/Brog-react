9c39a96f80e61415a536f71075ba178406140936
Brad Donakowski <braddonako@gmail.com>
2020-04-18 18:51:59 -0600

    terrible at commits today. Did styling on the home page

--- client/brogblog/src/Components/Home/index.js
+++ client/brogblog/src/Components/Home/index.js
@@ -4,18 +4,22 @@ class Home extends Component {
     render() {
         return (
             <div className='page_wrapper'>
-                
-                <div className='homeContainer'>
+                <div className='container'>
                     <div>
-                        <h1>Most recent BROG title</h1>
-                        <p>Here is some text just to fuckin put something in here. Soon we will connect to the DB to auto populate so my most recent post is always featured -- fill figure this out at somepoint lol. These are the fields we need tho</p>
+                        <h1 class='welcome'>Welcome to da BROG</h1>
+                        <p class='welcomeSub'>All about Coding Bootcamps && JavaScript </p>
                     </div>
-                    <div className='datePosted'>
-                        Date Posted: 4/18/2020
+                    <div className='homeContainer'>
+                        <div>
+                            <h1 className='postTitle'>Most recent BROG title</h1>
+                            <p className='postBody'>Here is some text just to fuckin put something in here. Soon we will connect to the DB to auto populate so my most recent post is always featured -- fill figure this out at somepoint lol. These are the fields we need tho</p>
+                        </div>
+                        <div className='datePosted'>
+                            Date Posted: 4/18/2020
+                        </div>
+                        
                     </div>
-                    
                 </div>
-
             </div>
         );
     }
