aefcedd6de9f426210491cd16cf4d71312f06c48
Brad Donakowski <braddonako@gmail.com>
2020-04-21 19:49:48 -0600

    Having trouble rendering data. Going to create an action for the articels

--- client/brogblog/src/Components/Home/index.js
+++ client/brogblog/src/Components/Home/index.js
@@ -3,13 +3,8 @@ import Axios from 'axios';
 
 class Home extends Component {
 
-    componentDidMount(){
-        Axios.get('/api/articles/show').then(response=>{
-            console.log(response)
-        })
-    }
-    
     render() {
+
         return (
             <div className='page_wrapper'>
                 <div className='container'>
@@ -24,7 +19,6 @@ class Home extends Component {
                             <p className='postBody'>
                                 {/* JUST FC*IN BUILD STUFF!!! When I first started out, I would see people on reddit saying this all the time, and its really fucking confusing. (At least I thought so, lol) I would think to myself 'What the hell am I going to build?'. Now that I am further along in the process, I realize how fricken true this is.   You just gotta do it. I know, I know. It sounds really simple, huh? */}
                                 Lorem Ipsum jest tekstem stosowanym jako przykładowy wypełniacz w przemyśle poligraficznym.Został po raz pierwszy użyty w XV w.przez nieznanego drukarza do wypełnienia tekstem próbnej książki.Pięć wieków później zaczął być używany przemyśle elektronicznym, pozostając praktycznie niezmienionym.Spopularyzował się w latach 60. XX w.wraz z publikacją arkuszy Letrasetu, zawierających fragmenty Lorem Ipsum, a ostatnio z zawierającym różne wersje Lorem Ipsum oprogramowaniem przeznaczonym do realizacji druków na komputerach osobistych, jak Aldus PageMaker
-
                             </p>
                             
                         </div>
