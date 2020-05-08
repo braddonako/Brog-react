11766579c6d771c6916ea96fbd235986c4d8f34d
Brad Donakowski <braddonako@gmail.com>
2020-05-08 12:59:53 -0600

    using a diffferent carousel

--- client/brogblog/src/Components/Logout/logout.js
+++ client/brogblog/src/Components/Logout/logout.js
@@ -1,11 +1,26 @@
 import React from 'react';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from 'react-responsive-carousel';
+
 
 const Logout = () => {
     return (
-        <div>
-            a ha ha
-        </div>
+             <Carousel>
+                <div>
+                    <img src="./IMG_2185.jpeg" />
+                    <p className="legend">Legend 1</p>
+                </div>
+                <div>
+                    <img src="assets/2.jpeg" />
+                    <p className="legend">Legend 2</p>
+                </div>
+                <div>
+                    <img src="assets/3.jpeg" />
+                    <p className="legend">Legend 3</p>
+                </div>
+            </Carousel>
     );
 };
+
 
 export default Logout;
