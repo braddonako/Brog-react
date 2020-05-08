ce2d46a02ec80870d64336b45e324ecfd755e419
Brad Donakowski <braddonako@gmail.com>
2020-05-08 01:49:08 -0600

    prob not the right route

--- server/server.js
+++ server/server.js
@@ -155,7 +155,7 @@ if(process.env.NODE_ENV === 'production'){
     const path = require('path');
 
     app.get('/*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, './client/brogblog/build/js', 'index.html'))
+        res.sendFile(path.resolve(__dirname, '/client/brogblog/build/js', 'index.html'))
     })
 }
 
