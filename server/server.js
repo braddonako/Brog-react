935f15286d75925d8ad7a4e605cd030d32a91424
Brad Donakowski <braddonako@gmail.com>
2020-05-08 01:09:50 -0600

    I am so close

--- server/server.js
+++ server/server.js
@@ -155,7 +155,7 @@ if(process.env.NODE_ENV === 'production'){
     const path = require('path');
 
     app.get('/*', (req, res) =>{
-        res.sendFile(path.resolve(__dirname, '../client/brogblog/src', 'index.js'))
+        res.sendFile(path.resolve(__dirname, '../client/brogblog/public', 'build', 'index.html'))
     })
 }
 
