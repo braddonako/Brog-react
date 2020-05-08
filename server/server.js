fed2e0b91fb5e8066808740df25e42fb31f8522c
Brad Donakowski <braddonako@gmail.com>
2020-05-07 23:10:41 -0600

    one more

--- server/server.js
+++ server/server.js
@@ -155,7 +155,7 @@ if(process.env.NODE_ENV === 'production'){
     const path = require('path');
 
     app.get('*', (req, res) =>{
-        res.sendfile(path.resolve(__dirname, '../client', 'index.html'))
+        res.sendfile(path.resolve(__dirname, '../client/public', 'index.html'))
     })
 }
 
